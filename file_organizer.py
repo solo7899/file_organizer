d67ca65610b3b235313c933eebc313841986cdd4
hamidreza <dolati.hamidreza78@gmail.com>
2025-07-07 02:18:33 +0330

    added progress bar now even dry runs make logs dry is not verbose by default now

--- file_organizer.py
+++ file_organizer.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 import shutil
 
+from alive_progress import alive_bar
 from pathlib import Path
 
 
@@ -16,8 +17,7 @@ EXTENSION_CATEGORIES = {
 
 
 def scan_directory(directory, verbose=False, dry_run=False, logfile=None):
-    if not dry_run:
-        logger.info(f"Scanning directory: {directory}")
+    logger.info(f"Scanning directory: {directory}")
     if verbose:
         print(f"Scanning directory: {directory}")
     directory = Path(directory)
@@ -35,25 +35,16 @@ def parse_arguments():
 
 def logging_setup(logname, dry_run):
     if dry_run:
-        return
+        format='Dry run: %(asctime)s - %(levelname)s - %(message)s'
+    else:
+        format='%(asctime)s - %(levelname)s - %(message)s'
     logging.basicConfig(
         filename=Path.cwd()/logname,
         level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s'
+        format=format
     )
     logger = logging.getLogger(__name__)
     return logger
-
-
-def log(logger, message, level=logging.INFO):
-    if level == logging.INFO:
-        logger.info(message)
-    elif level == logging.ERROR:
-        logger.error(message)
-    elif level == logging.DEBUG:
-        logger.debug(message)
-    else:
-        logger.warning(message)
 
 
 def categorize_file(file:Path):
@@ -66,7 +57,9 @@ def categorize_file(file:Path):
 
 def move(file:Path, file_category, verbose=False, dry_run=False):
     if dry_run:
-        print(f"Dry run: {file} would be moved to {file_category}")
+        if verbose:
+            print(f"{file} would be moved to {file_category}")
+        logger.info(f"{file} would be moved to {file_category}")
         return
     if not file_category.exists():
         Path(file_category).mkdir(parents=True, exist_ok=True)
@@ -85,9 +78,11 @@ def organize_files(files:list[Path], dry_run=False, verbose=False):
         return
 
     cwd = Path.cwd()
-    for file in files:
-        file_category = categorize_file(file)
-        move(file, cwd/file_category, verbose,dry_run)
+    with alive_bar(len(files)) as bar:
+        for file in files:
+            file_category = categorize_file(file)
+            move(file, cwd/file_category, verbose,dry_run)
+            bar()
 
 if __name__ == "__main__":
     args = parse_arguments()
@@ -108,9 +103,8 @@ if __name__ == "__main__":
         print("Files will be organized into directories based on their extensions.")
 
     files = scan_directory(args.directory, args.verbose, args.dry_run, logfile=args.logname)
-    if not args.dry_run:
-        logger.info(f"Found {len(files)} files in {args.directory}")
-    if args.verbose or args.dry_run:
+    logger.info(f"Found {len(files)} files in {args.directory}")
+    if args.verbose:
         print(f"Found {len(files)} files to organize.")
 
     organize_files(files, args.dry_run, args.verbose)
